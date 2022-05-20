25a3e4344f24b0eae532da048a7ace295583c098
KyungRyul <rudfbfz@gmail.com>
2022-05-20 11:46:09 +0900

    write_ok

--- phpProject/write_ok.php
+++ phpProject/write_ok.php
@@ -1,13 +1,34 @@
 <?php 
 
 include('common.php');
+$title = $_POST['title'];
+$content = $_POST['content'];
+$writer = $_SESSION['email'];
+$insertTime = date("Y-m-d H:i:s");
 
-print_r($_POST);
+$sql = "insert into board set
+        title = '$title',
+        content = '$content',
+        writer = '$writer',
+        insertTime = '$insertTime'
+        ";
 
+$result = $conn -> query($sql);
 
-
-
-
+if($result) {
+    echo "
+    <script>
+        location.href='index.php';
+    </script>
+    ";
+} else {
+    echo "
+    <script>
+        alert('글등록 실패.');
+        location.back();
+    </script>
+    ";
+}
 
 
 
